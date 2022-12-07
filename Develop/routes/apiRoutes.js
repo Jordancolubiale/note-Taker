c37bc6e465dfc96916062296af0097a1e564673d
Jordan Colubiale <jordancolubiale@Jordans-MacBook-Air-2.local>
2022-12-07 15:57:07 -0500

    added some apiRoutes with tutors help

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,2 +1,17 @@
-const path = require("path");
+const util = require("util");
+const fs = require("fs");
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 const router = require("express").Router();
+const db = "./db/db.json";
+
+router.get('/notes', (req, res) => {
+    return this.readFileAsync("./db/db.json", "utf8").then((notes) => {
+        let parseNotes = [].concat(Json.parse(notes));
+        return parseNotes
+    });
+});
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,14 +4,13 @@ const app = express();
 
 const PORT = process.env.PORT || 3001;
 
-
-app.use(express.static('public'));
-
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-
-
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
 
